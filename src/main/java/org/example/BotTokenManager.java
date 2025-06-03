94000842af419144a05be1200750776b7fe61424
Oneyoojin <dkffhdp1234@naver.com>
2025-06-03 23:56:23 +0900

    First Bot Commit

---
+++ src/main/java/org/example/BotTokenManager.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class BotTokenManager {
+    private String DiscordBotToken;
+
+    public BotTokenManager() {
+        settingToken();
+    }
+
+    private void settingToken() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("DiscordBotToken.propertises")) {
+            Properties properties = new Properties();
+            properties.load(input);
+            DiscordBotToken = properties.getProperty("Token");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getDiscordBotToken() {
+        return DiscordBotToken;
+    }
+}
---
+++ src/main/java/org/example/DiscordBot.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import net.dv8tion.jda.api.JDABuilder;
+
+public class DiscordBot {
+
+    public static void main(String[] args) {
+        BotTokenManager tokenManager = new BotTokenManager();
+        String token = tokenManager.getDiscordBotToken();
+        JDABuilder.createDefault(token)
+                .build();
+
+    }
+
+}
