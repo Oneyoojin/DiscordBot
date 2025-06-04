b4080e87d4eea0c301aa0f450e1e7a1d8732bd43
Oneyoojin <dkffhdp1234@naver.com>
2025-06-04 16:48:34 +0900

    디스코드 봇 기능추가.

--- src/main/java/org/example/DiscordBot.java
+++ src/main/java/org/example/DiscordBot.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 
 public class DiscordBot {
 
@@ -8,6 +9,12 @@ public class DiscordBot {
         BotTokenManager tokenManager = new BotTokenManager();
         String token = tokenManager.getDiscordBotToken();
         JDABuilder.createDefault(token)
+                .setActivity(Activity.competing("게임"))
+                .setActivity(Activity.playing("봇 만들기"))
+                .setActivity(Activity.playing("음악"))
+                .setActivity(Activity.streaming("방송","링크"))
+                .setActivity(Activity.watching("영화"))
+                .setActivity(Activity.customStatus("명령어 기다리는중..."))
                 .build();
 
     }
